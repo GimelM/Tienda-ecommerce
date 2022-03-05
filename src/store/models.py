2cd389a96ede8508d88efecd76daefb2f2507df1
Christian Medrano <christiangmedrano@hotmail.com>
2022-03-04 22:09:08 -0400

    Funcion para ordernar

--- src/store/models.py
+++ src/store/models.py
@@ -13,6 +13,9 @@ class Pets(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        ordering = ('-created_date',)
+    
     def __str__(self):
         return self.pet_name
 
